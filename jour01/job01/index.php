44717fdd90d90dff9f29c5fe94f584e1603d7bac
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-21 14:46:33 +0200

    val 6

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,9 +1,10 @@
 <?php
+//header('Content-Type: text/html; charset=utf-8');
 $str = "LaPlateforme";
 $str2 = "Vive";
 $str3 = "!";
-echo " $str2 . $str . $str3 "
-$val = 6;
-echo " $val "
+echo " $str2 $str $str3 ";
 
+$val = 6;
+echo $val;
 ?>
