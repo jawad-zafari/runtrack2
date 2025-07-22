472ff75ee7dba2672e22a57ff83efaa2e5955a25
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-22 10:19:28 +0200

    table added

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,5 +4,36 @@ $booleanVar = true;
 $integerVar = 42;
 $stringVar = "Hello";
 $floatVar = 3.14;
-
+//table
+echo "<table border='1'>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Name</th>
+                <th>Value</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+      </table>";
 ?>
