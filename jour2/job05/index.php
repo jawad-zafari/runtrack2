30c44381e53e6b2a09dd7ed6dc231abd256682d2
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-27 20:24:18 +0200

    sqrt

--- jour2/job05/index.php
+++ jour2/job05/index.php
@@ -1,4 +1,16 @@
 <?php
-for ($i = 2; $i <= 1000; $i++)
-        echo "$i<br />";
+function isPrime($num) {
+    if ($num <= 1) return false;
+    if ($num == 2) return true;
+    if ($num % 2 == 0) return false;
+    
+    for ($i = 3; $i <= sqrt($num); $i += 2) {
+        if ($num % $i == 0) return false;
+    }
+    return true;
+}
+
+for ($i = 2; $i <= 1000; $i++) {
+    if (isPrime($i)) {echo "$i<br />";}
+}
 ?>
