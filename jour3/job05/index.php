bdeb8f9c24e27e93ece8cae75006183ddcee4044
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-27 22:26:02 +0200

    table

---
+++ jour3/job05/index.php
@@ -0,0 +1,27 @@
+<?php
+$str = "On n'est pas le meilleur quand on le croit mais quand on le sait";
+$dir = array("consonnes" => 0, "voyelles" => 0);
+$vowels = array('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y');
+
+for ($i = 0; $i < strlen($str); $i++) {
+    $char = $str[$i];
+    if (ctype_alpha($char)) {if (in_array($char, $vowels)) {$dir["voyelles"]++;} 
+    else {$dir["consonnes"]++;}
+    }
+}
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+    <th>Lettres consonnes</th>
+    <th>Lettres voyelles</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $dir["consonnes"]; ?></td>
+            <td><?php echo $dir["voyelles"]; ?></td>
+        </tr>
+    </tbody>
+</table>
