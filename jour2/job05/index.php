a6044f8d4a97ccc67d6bf5a19017cc5975e9d53e
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-27 20:27:42 +0200

    isprime

--- jour2/job05/index.php
+++ jour2/job05/index.php
@@ -3,10 +3,7 @@ function isPrime($num) {
     if ($num <= 1) return false;
     if ($num == 2) return true;
     if ($num % 2 == 0) return false;
-    
-    for ($i = 3; $i <= sqrt($num); $i += 2) {
-        if ($num % $i == 0) return false;
-    }
+    for ($i = 3; $i <= sqrt($num); $i += 2) {if ($num % $i == 0) return false;}
     return true;
 }
 
