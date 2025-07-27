f385f3d11391a263bdb11dd70270d6b6bcd46e77
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-27 22:34:46 +0200

    if

--- jour3/job07/index.php
+++ jour3/job07/index.php
@@ -2,5 +2,6 @@
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
 $newStr = "";
 for ($i = 0; $i < strlen($str); $i++) {
-                          
+   if ($i == strlen($str) - 1) {
+                             
 ?>
