426cdfde2b6706ff14a447f46083f23967560645
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-28 14:27:06 +0200

    echo-str

--- jour3/job06/index.php
+++ jour3/job06/index.php
@@ -1,9 +1,5 @@
 <?php
 $str = "Les choses que l'on possede finissent par nous posseder.";
-$reversed = "";
-for ($i = strlen($str) - 1; $i >= 0; $i--) {
-    $reversed .= $str[$i];
-}
-
-echo $reversed;
+for ($i = strlen($str) - 1; $i >= 0; $i--) 
+echo $str[$i];
 ?>
