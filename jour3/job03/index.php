4febc7344e3a9cd37a7b6caaafb76fb123361803
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-28 12:52:06 +0200

    comp

--- jour3/job03/index.php
+++ jour3/job03/index.php
@@ -2,7 +2,7 @@
 $str = "I'm sorry Dave I'm afraid I can't do that.";
 $vowels = array('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
 for ($i = 0; $i < strlen($str); $i++) {
-    if (in_array($str[$i], $vowels)) {
+    if (in_array($str[$i],$vowels)) {
         echo $str[$i];
     }
 }
