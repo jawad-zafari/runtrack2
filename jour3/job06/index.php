590dc054eec4c6fde94fac6ab77d3148b897e694
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-28 16:34:06 +0200

    echo

--- jour3/job06/index.php
+++ jour3/job06/index.php
@@ -1,5 +1,6 @@
 <?php
 $str = "Les choses que l'on possede finissent par nous posseder.";
-for ($i = strlen($str) - 1; $i >= 0; $i--) 
+for ($i = strlen($str) - 1; $i >= 0; $i--)
+
 echo $str[$i];
 ?>
