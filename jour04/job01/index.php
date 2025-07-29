cbfd5f0d338b07861443b4531723ad93b2764d3a
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-29 10:02:59 +0200

    form

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -3,3 +3,8 @@ $count = count($_GET);
 echo "Le nombre d'argument GET envoyé est : " . $count;
 ?>
 
+<form>
+  <input type="text" name="prenom" placeholder="Prénom">
+  <input type="text" name="nom" placeholder="Nom">
+  <input type="submit" value="Envoyer">
+</form>
