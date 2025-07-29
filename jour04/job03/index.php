1f8843bbc51a6018706d57c2c14c481fe4f504c9
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-29 15:33:38 +0200

    POST

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,9 +1,9 @@
 <?php
 $count = count($_POST);
 if(isset($_POST)) {
-    echo  "Le nombre d'argument GET envoyé est : " . $count;
+    echo  "Le nombre d'argument POST envoyé est : " . $count;
   } 
-else { echo "« Aucun paramètre GET n'a été envoyé. »";}
+else { echo "« Aucun paramètre POST n'a été envoyé. »";}
 ?>
 <form method="POST" action="">
   <input type="text" name="prenom" placeholder="Prénom">
