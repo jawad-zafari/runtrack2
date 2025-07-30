2fbb1700cd4327c6f54b1a70c9ef212ff80e96a4
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-30 10:19:13 +0200

    nombre paire

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,14 +1,4 @@
 <?php
 if (isset($_GET['nombre'])) {
-    if ($_GET['nombre'] % 2 == 0) {
-        echo 'Nombre pair';
-    } else {
-        echo 'Nombre impair';
-    }
-}
-?>
-
-<form method="GET">
-  <input type="number" name="nombre" placeholder="Entrez un nombre" required>
-  <input type="submit" value="Vérifier">
-</form>
+    if ($_GET['nombre'] % 2 == 0) {echo 'Nombre pair';} 
+   
