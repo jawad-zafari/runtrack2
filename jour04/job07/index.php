8e44f332e76840f4c51152ef5e06b7aba9baa72f
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-31 09:16:18 +0200

    FORM

---
+++ jour04/job07/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Job 07 - Dessiner une maison</title>
+</head>
+<body>
+    <h2>Formulaire pour dessiner une maison</h2>
+    <form method="POST" action="index.php">
+        <label for="largeur">Largeur :</label>
+        <input type="text" name="largeur" id="largeur" required>
+        <br><br>
+        <label for="hauteur">Hauteur :</label>
+        <input type="text" name="hauteur" id="hauteur" required>
+        <br><br>
+        <input type="submit" value="Dessiner">
+    </form>
+
+    
