5c311e81c9baf2f3cbca2d2d7be350f616382660
jawad zafari <jawad.zafari@laplateforme.io>
2025-07-31 23:22:33 +0200

    function

---
+++ jour07/job05/index.php
@@ -0,0 +1,10 @@
+<?php
+function occurrences($str, $char) {
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] == $char) {
+            $count++;
+        }
+    }
+    return $count;
+}
