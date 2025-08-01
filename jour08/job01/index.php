18a5cf1e45cfbe0a9c4579927b3b6896b6ae95ba
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-01 09:37:34 +0200

    session-nbvisites

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -5,3 +5,14 @@ if (!isset($_SESSION['nbvisites'])) {
     $_SESSION['nbvisites'] = 0;
 }
 
+$_SESSION['nbvisites']++;
+
+echo "Nombre de vues : " . $_SESSION['nbvisites'];
+
+if (isset($_POST['reset'])) {
+    $_SESSION['nbvisites'] = 0;
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit;
+}
+?>
+
