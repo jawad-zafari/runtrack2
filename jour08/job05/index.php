6517326c6dbcb57e7b44a7764c64827cca693b12
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-01 11:34:21 +0200

    les diagonales

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -25,3 +25,14 @@ function check_winner($board) {
         }
     }
     
+// Vérifiez les diagonales
+    if ($board[0][0] != '-' && $board[0][0] == $board[1][1] && $board[1][1] == $board[2][2]) {
+        return $board[0][0];
+    }
+    if ($board[0][2] != '-' && $board[0][2] == $board[1][1] && $board[1][1] == $board[2][0]) {
+        return $board[0][2];
+    }
+    
+    return null;
+}
+
