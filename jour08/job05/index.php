7b2048e75fee5ed3599bb430711abc9418b299bd
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-01 11:32:11 +0200

    check winner

--- jour08/job05/index.php
+++ jour08/job05/index.php
@@ -10,3 +10,11 @@ if (!isset($_SESSION['board'])) {
     $_SESSION['current_player'] = 'X';
 }
 
+function check_winner($board) {
+    // Vérifier les lignes
+    for ($i = 0; $i < 3; $i++) {
+        if ($board[$i][0] != '-' && $board[$i][0] == $board[$i][1] && $board[$i][1] == $board[$i][2]) {
+            return $board[$i][0];
+        }
+    }
+    
