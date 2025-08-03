a53542605c7af4dea1bb2315180ed43d8b05e80c
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-03 22:39:34 +0200

    verifi conection

--- jour10/job01/index.php
+++ jour10/job01/index.php
@@ -2,3 +2,8 @@
 //Se connecter à la base de données
 $conn = mysqli_connect("localhost", "root", "", "jour09");
 
+// Vérifier la connexion
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
