9fb867eb419123aa0d91e29cebab6570373c7fad
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 09:20:25 +0200

    thead-table

--- jour10/job03/index.php
+++ jour10/job03/index.php
@@ -5,3 +5,10 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql = "SELECT prenom, nom, naissance FROM etudiants WHERE sexe = 'Femme'";
+$result = mysqli_query($conn, $sql);
+
+echo "<table border='1'>";
+echo "<thead><tr><th>Prénom</th><th>Nom</th><th>Naissance</th></tr></thead>";
+echo "<tbody>";
+
