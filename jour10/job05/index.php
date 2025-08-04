454bed6b98b9b204ac1fe57e228bdde1d03218a2
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 09:32:30 +0200

    sql-select

--- jour10/job05/index.php
+++ jour10/job05/index.php
@@ -5,3 +5,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql = "SELECT * FROM etudiants WHERE DATEDIFF(CURRENT_DATE, naissance)/365 < 18";
+$result = mysqli_query($conn, $sql);
+
