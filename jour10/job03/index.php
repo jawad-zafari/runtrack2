182136b83f718ac537991b860c68ef5b223c5d05
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 09:21:26 +0200

    echo mysql

--- jour10/job03/index.php
+++ jour10/job03/index.php
@@ -12,3 +12,15 @@ echo "<table border='1'>";
 echo "<thead><tr><th>Prénom</th><th>Nom</th><th>Naissance</th></tr></thead>";
 echo "<tbody>";
 
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['prenom'] . "</td>";
+    echo "<td>" . $row['nom'] . "</td>";
+    echo "<td>" . $row['naissance'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</tbody></table>";
+
+mysqli_close($conn);
+?>
