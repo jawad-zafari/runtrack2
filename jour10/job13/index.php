d0e6bd817472c6dfb5c626a289ec88e24de50a83
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:51:47 +0200

    sql-select

--- jour10/job13/index.php
+++ jour10/job13/index.php
@@ -5,3 +5,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql = "SELECT salles.nom AS nom_salle, etages.nom AS nom_etage FROM salles JOIN etages ON salles.id_etage = etages.id";
+$result = mysqli_query($conn, $sql);
+
