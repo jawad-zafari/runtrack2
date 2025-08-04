a5397819b9e56349b57c6ae5d262b1cee1f7de25
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:44:24 +0200

    table-thead-td-echo

--- jour10/job09/index.php
+++ jour10/job09/index.php
@@ -8,3 +8,20 @@ if (!$conn) {
 $sql = "SELECT * FROM salles ORDER BY capacite DESC";
 $result = mysqli_query($conn, $sql);
 
+echo "<table border='1'>";
+echo "<thead><tr><th>ID</th><th>Nom</th><th>ID étage</th><th>Capacité</th></tr></thead>";
+echo "<tbody>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['nom'] . "</td>";
+    echo "<td>" . $row['id_etage'] . "</td>";
+    echo "<td>" . $row['capacite'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</tbody></table>";
+
+mysqli_close($conn);
+?>
