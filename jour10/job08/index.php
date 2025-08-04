df80b2fa67d8ec2637d2147480a6807471478900
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:42:12 +0200

    capacité total

--- jour10/job08/index.php
+++ jour10/job08/index.php
@@ -5,3 +5,8 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql = "SELECT SUM(capacite) AS capacite_totale FROM salles";
+$result = mysqli_query($conn, $sql);
+
+;
+?>
