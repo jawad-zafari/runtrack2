21f04a8449c7a121fe03eff4c9401a176fb3d5d1
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:49:17 +0200

    table-echo

--- jour10/job12/index.php
+++ jour10/job12/index.php
@@ -8,3 +8,19 @@ if (!$conn) {
 $sql = "SELECT prenom, nom, naissance FROM etudiants WHERE YEAR(naissance) BETWEEN 1998 AND 2018";
 $result = mysqli_query($conn, $sql);
 
+echo "<table border='1'>";
+echo "<thead><tr><th>Prénom</th><th>Nom</th><th>Naissance</th></tr></thead>";
+echo "<tbody>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['prenom'] . "</td>";
+    echo "<td>" . $row['nom'] . "</td>";
+    echo "<td>" . $row['naissance'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</tbody></table>";
+
+mysqli_close($conn);
+?>
