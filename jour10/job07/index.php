1ab5b8935558b032aca82645cf71628a6d168d02
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 11:35:12 +0200

    modifi capacite

--- jour10/job07/index.php
+++ jour10/job07/index.php
@@ -4,17 +4,16 @@ $conn = mysqli_connect("localhost", "root", "", "jour09");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-$sql = "SELECT SUM(superficie) AS superficie_totale FROM etages";
+$sql = "SELECT SUM(capacite) AS capacite_totale FROM salles";
 $result = mysqli_query($conn, $sql);
 
 echo "<table border='1'>";
-echo "<thead><tr><th>Superficie totale</th></tr></thead>";
+echo "<thead><tr><th>Capacité totale</th></tr></thead>";
 echo "<tbody>";
 
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
-    echo "<td>" . $row['superficie_totale'] . "</td>";
+    echo "<td>" . $row['capacite_totale'] . "</td>";
     echo "</tr>";
 }
 
