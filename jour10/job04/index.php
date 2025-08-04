4b1befd9f899bd9e4a1907f75573c017687d0a9f
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 09:27:01 +0200

    thead

--- jour10/job04/index.php
+++ jour10/job04/index.php
@@ -12,17 +12,3 @@ echo "<table border='1'>";
 echo "<thead><tr><th>Prénom</th><th>Nom</th><th>Naissance</th><th>Sexe</th><th>Email</th></tr></thead>";
 echo "<tbody>";
 
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['prenom'] . "</td>";
-    echo "<td>" . $row['nom'] . "</td>";
-    echo "<td>" . $row['naissance'] . "</td>";
-    echo "<td>" . $row['sexe'] . "</td>";
-    echo "<td>" . $row['email'] . "</td>";
-    echo "</tr>";
-}
-
-echo "</tbody></table>";
-
-mysqli_close($conn);
-?>
