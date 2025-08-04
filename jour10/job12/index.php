b8a4e1cf72a5c75ddef39c54673df5d467b7c6f2
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:49:02 +0200

    sql-select

--- jour10/job12/index.php
+++ jour10/job12/index.php
@@ -5,4 +5,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$sql = "SELECT prenom, nom, naissance FROM etudiants WHERE YEAR(naissance) BETWEEN 1998 AND 2018";
+$result = mysqli_query($conn, $sql);
 
