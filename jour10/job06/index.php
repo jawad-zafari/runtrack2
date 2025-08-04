c091f1672a3ee8e944d0240947d3fb24c4a2759d
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:06:45 +0200

    echo table

--- jour10/job06/index.php
+++ jour10/job06/index.php
@@ -8,3 +8,17 @@ if (!$conn) {
 $sql = "SELECT COUNT(*) AS nb_etudiants FROM etudiants";
 $result = mysqli_query($conn, $sql);
 
+echo "<table border='1'>";
+echo "<thead><tr><th>Nombre d'étudiants</th></tr></thead>";
+echo "<tbody>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['nb_etudiants'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</tbody></table>";
+
+mysqli_close($conn);
+?>
