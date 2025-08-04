33a8ed8c7b65a32440aca44ed3d9c5d410552006
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 09:49:57 +0200

    select from etudiants

--- jour10/job05/index.php
+++ jour10/job05/index.php
@@ -6,23 +6,3 @@ if (!$conn) {
 }
 
 $sql = "SELECT * FROM etudiants WHERE DATEDIFF(CURRENT_DATE, naissance)/365 < 18";
-$result = mysqli_query($conn, $sql);
-
-echo "<table border='1'>";
-echo "<thead><tr><th>Prénom</th><th>Nom</th><th>Naissance</th><th>Sexe</th><th>Email</th></tr></thead>";
-echo "<tbody>";
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['prenom'] . "</td>";
-    echo "<td>" . $row['nom'] . "</td>";
-    echo "<td>" . $row['naissance'] . "</td>";
-    echo "<td>" . $row['sexe'] . "</td>";
-    echo "<td>" . $row['email'] . "</td>";
-    echo "</tr>";
-}
-
-echo "</tbody></table>";
-
-mysqli_close($conn);
-?>
