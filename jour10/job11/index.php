7116ede1546082e31e9aa203b77f7f72ecfec960
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:48:04 +0200

    echo table-echo capacité

--- jour10/job11/index.php
+++ jour10/job11/index.php
@@ -8,3 +8,17 @@ if (!$conn) {
 $sql = "SELECT AVG(capacite) AS capacite_moyenne FROM salles";
 $result = mysqli_query($conn, $sql);
 
+echo "<table border='1'>";
+echo "<thead><tr><th>Capacité moyenne</th></tr></thead>";
+echo "<tbody>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['capacite_moyenne'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</tbody></table>";
+
+mysqli_close($conn);
+?>
