613bc023304c26fce0c7bb1b1cbc2cf35343a3fb
jawad zafari <jawad.zafari@laplateforme.io>
2025-08-04 10:32:19 +0200

    select superficie

--- jour10/job07/index.php
+++ jour10/job07/index.php
@@ -8,17 +8,3 @@ if (!$conn) {
 $sql = "SELECT SUM(superficie) AS superficie_totale FROM etages";
 $result = mysqli_query($conn, $sql);
 
-echo "<table border='1'>";
-echo "<thead><tr><th>Superficie totale</th></tr></thead>";
-echo "<tbody>";
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['superficie_totale'] . "</td>";
-    echo "</tr>";
-}
-
-echo "</tbody></table>";
-
-mysqli_close($conn);
-?>
